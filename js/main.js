80b8b4cbbe0679cb5f4c5c5dc7642f3b8fd8cb50
WEBABDU <rabdufattox@mail.ru>
2021-12-13 23:12:04 +0500

    modal added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ $(document).ready(function () {
   let counterUp = $(".counter-up");
   let counterDown = $(".counter-down");
   let currentPath = $(".home-image path");
+  let modal = $(".modal");
+  let lookFlat = $(".button-primary");
+  let modalClose = $(".modal-close-button");
+  let flatPath = $(".flats path");
+  let currentFlat = 0;
+  let flatLink = $(".flat-link");
+
+  currentPath.on("click", toogleClass);
+  lookFlat.on("click", toogleClass);
+  modalClose.on("click", toogleClass);
+
   currentPath.on("mouseover", function () {
     currentPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
@@ -35,4 +46,22 @@ $(document).ready(function () {
       $(`[data-floor=${currentFloor}]`).toggleClass("current-floor");
     }
   });
+
+  flatPath.on("mouseover", function () {
+    flatLink.removeClass("current-flat");
+    flatPath.removeClass("current-link");
+    currentFlat = $(this).attr("data-flat");
+    $(`[data-house=${currentFlat}]`).toggleClass("current-flat");
+  });
+
+  flatLink.on("mouseover", function () {
+    flatPath.removeClass("current-link");
+    flatLink.removeClass("current-flat");
+    currentFlat = $(this).attr("data-house");
+    $(`[data-flat=${currentFlat}]`).toggleClass("current-link");
+  });
+
+  function toogleClass() {
+    modal.toggleClass("is-open");
+  }
 });
